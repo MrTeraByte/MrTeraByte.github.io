f78da50b23c92ec52dc4344d0046e29292500d1d
MrTeraByte <92567869+MrTeraByte@users.noreply.github.com>
2022-04-25 13:55:56 +0600

    Update script.js
    Updated the codebase 1.0

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ $(document).ready( () => {
 
 	let introtxt = $("p:first");
 	
-    introtxt.css( "left", (screen.width / 2 +  - 90) + "px" )
+    introtxt.css( "left", (screen.width / 2 +  - 100) + "px" )
     .css("display","none")
     .fadeIn(2000,() => {
     	$("#intro").fadeOut(1000, () => {
-    		$("#note").css("display","block");
+    		$("#note").fadeIn();
+    			$("#intro").remove();
     	});
     })
 });
