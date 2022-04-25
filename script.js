d2c508c886cd434b2a332e6ef8b09707d8b43666
MrTeraByte <92567869+MrTeraByte@users.noreply.github.com>
2022-04-25 14:13:08 +0600

    Made intro responsive(js)
    Updating js codebase for responsive intro

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ $(document).ready( () => {
 
 	let introtxt = $("p:first");
 	
-    introtxt.css( "left", (screen.width / 2 +  - 100) + "px" )
-    .css("display","none")
-    .fadeIn(2000,() => {
+    introtxt.css("display","none").fadeIn(2000,() => {
     	$("#intro").fadeOut(1000, () => {
     		$("#note").fadeIn();
     			$("#intro").remove();
